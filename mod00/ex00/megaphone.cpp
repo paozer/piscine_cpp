4f47489ee9bfaf4932259dcabd4e24f8eca5ce57
paozer <p.ramellaratin@gmail.com>
2020-06-21 17:17:34 +0200

    headers, formatting and gitignore

---
+++ mod00/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pramella <pramella@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/21 17:13:49 by pramella          #+#    #+#             */
+/*   Updated: 2020/06/21 17:13:50 by pramella         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int		main(int ac, char *av[])
+{
+	if (ac == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	}
+	else {
+		for (int i = 1; av[i] != nullptr; ++i) {
+			for (int j = 0; av[i][j] != '\0'; ++j) {
+				av[i][j] = toupper(av[i][j]);
+			}
+			std::cout << av[i];
+		}
+		std::cout << '\n';
+	}
+	return (0);
+}
---
+++ mod00/ex01/Contact.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pramella <pramella@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/21 17:14:53 by pramella          #+#    #+#             */
+/*   Updated: 2020/06/21 17:14:55 by pramella         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <iostream>
+# include <iomanip>
+
+class Contact {
+	private:
+		std::string first_name;
+		std::string last_name;
+		std::string nickname;
+		std::string login;
+		std::string postal_address;
+		std::string email_address;
+		std::string phone_number;
+		std::string birthday_date;
+		std::string favorite_meal;
+		std::string underwear_color;
+		std::string darkest_secret;
+
+	public:
+		Contact();
+		Contact(std::string info[]);
+		void str(void);
+		std::string get_first_name(void);
+		std::string get_last_name(void);
+		std::string get_login(void);
+};
+
+#endif
