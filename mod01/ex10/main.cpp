46ebcc3fe8f3ddad19f6291965fedaf6b9dde1b9
paozer <p.ramellaratin@gmail.com>
2020-06-24 22:23:18 +0200

    ex10: need ctrl-d on non terminated line handling

---
+++ mod01/ex10/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+int main(int ac, char *av[])
+{
+	std::string output;
+
+	if (ac == 1) {
+		while (std::getline(std::cin, output)) {
+			std::cout << output << std::endl;
+		}
+	}
+	else {
+		for (int i = 1; av[i] != nullptr; ++i) {
+			std::ifstream file (av[i], std::ifstream::in);
+			if (file.fail()) {
+				std::cerr << "cato9tails: "<< av[i] << ": No such file or directory\n";
+				continue ;
+			}
+			while (std::getline(file, output))
+				std::cout << output << std::endl;
+		}
+	}
+}
