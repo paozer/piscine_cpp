a6a9de337ae9d52c35679a588c3f25a756a478f3
paozer <p.ramellaratin@gmail.com>
2020-06-24 19:14:14 +0200

    ex08 finished

---
+++ mod01/ex08/Human.cpp
@@ -0,0 +1,31 @@
+#include "Human.hpp"
+
+void Human::meleeAttack(std::string const& target)
+{
+	std::cout << "Melee attack on " << target << std::endl;
+}
+
+void Human::rangedAttack(std::string const& target)
+{
+	std::cout << "Ranged attack on " << target << std::endl;
+}
+
+void Human::intimidatingShout(std::string const& target)
+{
+	std::cout << "Intimidating shout towards " << target << std::endl;
+}
+
+void Human::action(std::string const& action_name, std::string const& target)
+{
+	std::string action[3] = {"meleeAttack", "rangedAttack", "intimidatingShout"};
+	// typedef memFuncPtr is pointer to Human member function which takes cons string
+	typedef void (Human::*memFuncPtr) (std::string const& target);
+	memFuncPtr mfp[] = { &Human::meleeAttack, &Human::rangedAttack, &Human::intimidatingShout };
+
+	for (int i = 0; i < 3; ++i) {
+		if (action_name == action[i]) {
+			(this->*mfp[i]) (target);
+			return ;
+		}
+	}
+}
---
+++ mod01/ex08/main.cpp
@@ -0,0 +1,11 @@
+#include "Human.hpp"
+
+int main(void)
+{
+	Human human;
+
+	human.action("meleeAttack", "thomas");
+	human.action("rangedAttack", "eric");
+	human.action("intimidatingShout", "maria");
+	return (0);
+}
