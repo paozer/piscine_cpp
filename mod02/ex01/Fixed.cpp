443cf386909eac4211428880ab52136ecc5c5c5e
paozer <p.ramellaratin@gmail.com>
2020-06-27 17:12:04 +0200

    mod02 ex01 finished

---
+++ mod02/ex01/Fixed.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pramella <pramella@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/27 17:11:27 by pramella          #+#    #+#             */
+/*   Updated: 2020/06/27 17:11:28 by pramella         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixed_point_value(0) {}
+
+// moves bits up binary_point positions
+Fixed::Fixed(const int nb) : fixed_point_value(nb * (1 << binary_point)) {}
+
+// loss of precision
+Fixed::Fixed(const float nb) : fixed_point_value(roundf(nb * (1 << binary_point))) {}
+
+Fixed::~Fixed() {}
+
+Fixed::Fixed(const Fixed& other) { fixed_point_value = other.getRawBits(); }
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+	fixed_point_value = other.getRawBits();
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const { return (fixed_point_value); }
+
+void Fixed::setRawBits(int const raw) { fixed_point_value = raw;}
+
+int Fixed::toInt(void) const { return (roundf(this->toFloat())); }
+
+// undoes int constructor result
+float Fixed::toFloat(void) const
+{ return ((float)this->getRawBits() / (float)(1 << binary_point)); }
+
+std::ostream& operator<<(std::ostream& os, const Fixed& f) { return (os << f.toFloat()); }
---
+++ mod02/ex01/Fixed.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pramella <pramella@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/27 17:11:32 by pramella          #+#    #+#             */
+/*   Updated: 2020/06/27 17:11:32 by pramella         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <fstream>
+# include <cmath>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const int nb);
+		Fixed(const float nb);
+		~Fixed();
+
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+
+		int toInt(void) const;
+		float toFloat(void) const;
+
+	private:
+		int fixed_point_value;
+		static const int binary_point = 8;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& f);
+
+#endif
