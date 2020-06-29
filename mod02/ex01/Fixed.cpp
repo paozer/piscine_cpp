fb77f593d4e287fa1b9ff5d181bde90e67cfc04b
paozer <p.ramellaratin@gmail.com>
2020-06-29 16:57:32 +0200

    mod02 ex01 cleanup

--- mod02/ex01/Fixed.cpp
+++ mod02/ex01/Fixed.cpp
@@ -15,18 +15,18 @@
 Fixed::Fixed() : fixed_point_value(0) {}
 
 // moves bits up binary_point positions
-Fixed::Fixed(const int nb) : fixed_point_value(nb * (1 << binary_point)) {}
+Fixed::Fixed(const int nb) : fixed_point_value(nb << binary_point) {}
 
 // loss of precision
 Fixed::Fixed(const float nb) : fixed_point_value(roundf(nb * (1 << binary_point))) {}
 
 Fixed::~Fixed() {}
 
-Fixed::Fixed(const Fixed& other) { fixed_point_value = other.getRawBits(); }
+Fixed::Fixed(const Fixed& other) { this->setRawBits(other.getRawBits()); }
 
 Fixed& Fixed::operator=(const Fixed& other)
 {
-	fixed_point_value = other.getRawBits();
+	this->setRawBits(other.getRawBits());
 	return (*this);
 }
 
@@ -34,10 +34,9 @@ int Fixed::getRawBits(void) const { return (fixed_point_value); }
 
 void Fixed::setRawBits(int const raw) { fixed_point_value = raw;}
 
-int Fixed::toInt(void) const { return (roundf(this->toFloat())); }
+int Fixed::toInt(void) const { return (roundf(this->getRawBits() >> binary_point)); }
 
 // undoes int constructor result
-float Fixed::toFloat(void) const
-{ return ((float)this->getRawBits() / (float)(1 << binary_point)); }
+float Fixed::toFloat(void) const { return ((float)this->getRawBits() / (float)(1 << binary_point)); }
 
 std::ostream& operator<<(std::ostream& os, const Fixed& f) { return (os << f.toFloat()); }
