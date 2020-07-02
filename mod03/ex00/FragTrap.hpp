07e4d4d3431d6bd2e03aedcf512de37bbbb77cb0
paozer <p.ramellaratin@gmail.com>
2020-07-02 03:18:19 +0200

    mod03 ex00 finished

---
+++ mod03/ex00/FragTrap.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pramella <pramella@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/02 03:17:43 by pramella          #+#    #+#             */
+/*   Updated: 2020/07/02 03:17:44 by pramella         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+#include <string>
+#include <iostream>
+#include <cstdlib> // rand(), srand()
+
+class FragTrap
+{
+	public:
+		FragTrap();
+		FragTrap(std::string name);
+		FragTrap(const FragTrap&);
+		~FragTrap();
+		FragTrap& operator=(const FragTrap&);
+
+		void rangedAttack(std::string const& target);
+		void meleeAttack(std::string const& target);
+		void vaulthunter_dot_exe(std::string const& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+
+	private:
+		std::string _name;
+		unsigned int _hit_points;
+		unsigned int _max_hit_points;
+		unsigned int _energy_points;
+		unsigned int _max_energy_points;
+		unsigned int _level;
+		unsigned int _melee_attack_damage;
+		unsigned int _ranged_attack_damage;
+		unsigned int _armor_damage_reduction;
+};
+
+#endif
---
+++ mod03/ex00/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pramella <pramella@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/02 03:17:47 by pramella          #+#    #+#             */
+/*   Updated: 2020/07/02 03:17:48 by pramella         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+int main(void)
+{
+	srand(time(NULL));
+	FragTrap f0 ("f0");
+	FragTrap f1 ("f1");
+	FragTrap f2 ("f2");
+	std::cout << std::endl;
+
+	f0.vaulthunter_dot_exe("a troll");
+	f1.meleeAttack("the world");
+	f2.rangedAttack("the c++ piscine");
+	std::cout << std::endl;
+
+	f0.takeDamage(4);
+	f1.takeDamage(40);
+	f1.takeDamage(200);
+	f2.takeDamage(99);
+	std::cout << std::endl;
+
+	f0.beRepaired(9000);
+	f1.beRepaired(11);
+	f2.beRepaired(94);
+	std::cout << std::endl;
+	return (0);
+}
