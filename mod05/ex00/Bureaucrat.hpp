0c7aa95a31f485975fe544e1383a12f7ddd74f33
paozer <p.ramellaratin@gmail.com>
2020-07-10 00:46:03 +0200

    mod05 ex00 finished

---
+++ mod05/ex00/Bureaucrat.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <exception>
+
+class Bureaucrat
+{
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat&);
+		~Bureaucrat();
+		Bureaucrat& operator=(const Bureaucrat&);
+
+		Bureaucrat& operator++();
+		Bureaucrat& operator--();
+
+		std::string getName() const;
+		int getGrade() const;
+
+		struct GradeToLow: public std::exception {};
+		struct GradeToHigh: public std::exception {};
+
+	private:
+		const std::string _name;
+		int _grade;
+};
+
+std::ostream& operator<<(std::ostream& os, Bureaucrat& b);
+
+#endif
---
+++ mod05/ex00/main.cpp
@@ -0,0 +1,53 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+	Bureaucrat* tom;
+
+	try {
+		tom = new Bureaucrat("tom", 314);
+	}
+	catch (std::exception& e) {
+		std::cout << "[OK] caught grade 314 creation" << std::endl;
+	}
+
+	try {
+		tom = new Bureaucrat("tom", 0);
+	}
+	catch (std::exception& e) {
+		std::cout << "[OK] caught grade 0 creation" << std::endl;
+	}
+
+	try {
+		tom = new Bureaucrat("tom", -999);
+	}
+	catch (Bureaucrat::GradeToHigh& e) {
+		std::cout << "[OK] caught grade -999 creation" << std::endl;
+	}
+
+	try {
+		tom = new Bureaucrat("tom", 1);
+		std::cout << std::endl;
+	}
+	catch (Bureaucrat::GradeToHigh& e) {
+		std::cout << "[ERROR] this shouldn't be displayed" << std::endl;
+	}
+
+	std::cout << *tom << std::endl;
+	int i;
+	for (i = 0; i < 200; ++i)
+		--*tom;
+	std::cout << "after " << i << " decrementation: " << *tom;
+	for (i = 0; i < 200; ++i)
+		++*tom;
+	std::cout << "after " << i << " incrementation: " << *tom;
+	std::cout << std::endl;
+	for (i = 0; i < 42; ++i)
+		--*tom;
+	std::cout << "after " << i << " decrementation: " << *tom;
+	for (i = 0; i < 42; ++i)
+		++*tom;
+	std::cout << "after " << i << " incrementation: " << *tom;
+	delete tom;
+	return (0);
+}
